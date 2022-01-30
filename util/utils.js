19096248d38fc22282537abca6071ef878725090
Vrezerino <parkpatrick1@gmail.com>
2022-01-31 01:57:44 +0200

    Edit serverside handling

--- util/utils.js
+++ util/utils.js
@@ -1,7 +1,7 @@
 const badwords = [
 	'http://', 'https://', 'ftp://', 'spotify:', 'nigger', 'n1gger', 
 	'nígger', 'gook', 'chink', 'retard', 'r3t4rd', 'r3tard', 
-	'fuck', 'shit', 'sh1t', 'porn'
+	'fuck', 'shit', 'sh1t', 'porn', 'kike', 'k1ke'
 ];
 const passesFilter = msg => !badwords.some(badword => msg.toLowerCase().includes(badword));
 
