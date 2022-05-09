128333a4448b713be765f0ab2968d56f1350b1e3
Patrick Park <60422113+Vrezerino@users.noreply.github.com>
2022-05-09 14:49:49 +0300

    Update utils.js

--- util/utils.js
+++ util/utils.js
@@ -1,8 +1,4 @@
-const badwords = [
-	'http://', 'https://', 'ftp://', 'spotify:', 'nigger', 'n1gger', 
-	'nígger', 'gook', 'chink', 'retard', 'r3t4rd', 'r3tard', 
-	'fuck', 'shit', 'sh1t', 'porn', 'kike', 'k1ke'
-];
+//const badwords = [censored];
 const passesFilter = msg => !badwords.some(badword => msg.toLowerCase().includes(badword));
 
 exports.passesFilter = passesFilter;
